71143e937a46e1551cee8d93a8412a0e08ca4f02
allen <yilangliang@wustl.edu>
2024-07-10 22:39:15 +0800

    add MainLayout.tsx

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
+import { use } from 'react';
+import { server } from 'typescript';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack: (config, { isServer }) => {
+        config.module.rules.push({test: /\.nodes$/, use: 'raw-loader' })
+
+        if (!isServer) config.externals.push('canvas');
+        return config;
+    }
+};
 
 export default nextConfig;
